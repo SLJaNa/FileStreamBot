de256e709e7247160958e4e2a6d553233bf95077
SL_JaNa <93564410+SLJaNa@users.noreply.github.com>
2021-11-05 13:27:34 +0530

    Update __init__.py

--- WebStreamer/utils/__init__.py
+++ WebStreamer/utils/__init__.py
@@ -1,3 +1,3 @@
-# This file is a part of TG-FileStreamBot
-# Coding : Jyothis Jayanth [@EverythingSuckz]
-# Maintained By : Avishkar Patil [ @AvishkarPatil ] [ Telegram ]
+# This file is a part of SL_Jana_File to URL Uploader_Bot
+# Coding : 𝙎𝙇_𝙅𝙖𝙣𝙖_𝙏𝙚𝙖𝙢 [@SL_Jana_Team]
+# Maintained By : 𝙎𝙇_𝙅𝙖𝙣𝙖_𝙏𝙚𝙖𝙢 [ @SL_Jana_Team ] [ Telegram ]
